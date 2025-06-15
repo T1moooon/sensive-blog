194b389d8b531e2ac8d88622b99b7d9dab6ce651
Timon <74593747+T1moooon@users.noreply.github.com>
2025-06-15 20:45:54 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -43,12 +43,13 @@ def index(request):
     most_popular_posts = (
         Post.objects
             .popular()[:5]
+            .prefetch_related('tags')
             .fetch_with_comments_count()
     )
     most_fresh_posts = (
         Post.objects
             .order_by('-published_at')
-            .prefetch_related('author')[:5]
+            .prefetch_related('author', 'tags')[:5]
             .fetch_with_comments_count()
     )
     most_popular_tags = Tag.objects.popular()[:5]
